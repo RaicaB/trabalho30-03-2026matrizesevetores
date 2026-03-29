2375bda6f55f85c746bf66a79f5ac7c983c43019
RaicaB <raicamartins97@gmail.com>
2026-03-29 14:29:29 -0300

    Add files via upload

---
+++ ex3matrizevetores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int linhas = 3;
+        int colunas = 5;
+        int[,] matriz = new int[linhas, colunas];
+
+        Console.WriteLine("Digite os 15 números para preencher a matriz 3x5:");
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Elemento [{i},{j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("\nSoma dos valores de cada linha:");
+        for (int i = 0; i < linhas; i++)
+        {
+            int soma = 0;
+            for (int j = 0; j < colunas; j++)
+            {
+                soma += matriz[i, j];
+            }
+            Console.WriteLine($"Linha {i}: {soma}");
+        }
+    }
+}
